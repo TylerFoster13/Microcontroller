3b4f94f3f67fdfccf586087010f797495e1d32ae
TylerFoster13 <tyler@jamesjerkinscomputer.com>
2017-06-16 08:44:12 -0500

    Add files via upload

---
+++ Hello World/Hello World.c
@@ -0,0 +1,12 @@
+/*
+ * Tyler Foster
+ * Program # 1
+ * Hello World
+ * 6/16/17
+*/
+#include "simpletools.h"                      // Include simple tools
+
+int main()                                    // Main function
+{
+print("Hello World.") ;
+}
